6c949e3b1ef37e4a9196a4ba71d91c9231fdc321
USUARIO <Mario_cane.do@hotmail.com>
2024-04-11 23:29:05 -0400

    Merge remote-tracking branch 'origin/master'

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.myapplication;
 
+
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -11,16 +14,17 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // infla vista
+        // inflate vista
         setContentView(R.layout.activity_main);
 
 
 
-        // cambiar elementos a EditText
-        // recibir datos desde layot
+        // new  elementos a EditText
+        // reciter elementos of layout
         EditText txt1 =findViewById(R.id.num1);
         EditText txt2 = findViewById(R.id.num2);
         TextView txt3 = findViewById(R.id.resul);
@@ -36,19 +40,26 @@ public class MainActivity extends AppCompatActivity {
 
               String num1 = txt1.getText().toString();
               String num2 = txt2.getText().toString();
+              try {
+                  if (txt1.getText().toString().isEmpty() || txt2.getText().toString().isEmpty()) {
+                      Toast mensaje = Toast.makeText(getApplicationContext(), "INGRESE TODOS LOS VALORES", Toast.LENGTH_LONG);
+                      mensaje.show();
 
-              if(txt1.getText().toString().isEmpty() || txt2.getText().toString().isEmpty())
-              {
-                  Toast mensaje = Toast.makeText(getApplicationContext(),"INGRESE TODOS LOS VALORES",Toast.LENGTH_LONG);
-                  mensaje.show();
+
+                  }
+
+                  int resul = Integer.parseInt(num1) + Integer.parseInt(num2);
+                  txt3.setText("El resultado es :" + resul);
+                  txt1.setText("");
+                  txt2.setText("");
+              } catch (Exception e) {
+
+                  Log.v("ver"," ALGO SALIO MAL");
+
 
               }
-
-              int resul = Integer.parseInt(num1)+ Integer.parseInt(num2);
-              txt3.setText("El resultado es :"+ resul);
-              txt1.setText("");
-              txt2.setText("");
           }
+
       });
 
 
